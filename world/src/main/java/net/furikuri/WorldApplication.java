dbd54b4d8e90a78930de8ffb54b9ea977d4d0579
Theo Pack Test <tf.pack@gmail.com>
2016-02-27 18:47:39 +0100

    Add world service

---
+++ world/src/main/java/net/furikuri/WorldApplication.java
@@ -0,0 +1,22 @@
+package net.furikuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class WorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "World";
+    }
+}
